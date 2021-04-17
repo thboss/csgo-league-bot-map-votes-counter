7076b8c4403ecf058882b5fab7103951fed84435
TheBO$$ <40491968+thboss@users.noreply.github.com>
2021-04-17 14:26:35 +0300

    Revert "Revert "Stop hibernating game server when a match starts""
    This reverts commit bf2cc1a06460c9718d45e80ed3073e26d64784ef.

--- bot/cogs/utils/api.py
+++ bot/cogs/utils/api.py
@@ -293,6 +293,7 @@ class ApiHelper:
             'players_per_team': len(team_one),
             'min_players_to_ready': len(team_two),
             'match_cvars': {
+                'sv_hibernate_when_empty': 0,
                 'get5_time_to_start': 300,  # warmup 5 minutes
                 'get5_kick_when_no_match_loaded': 1
             }
